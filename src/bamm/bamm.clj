c3b97d738cf4488b46453025efad0ccc882d5a25
Robert P. Levy <r.p.levy@gmail.com>
2013-05-12 21:21:05 -0700

    added embedding of legend info in SVG

--- src/bamm/bamm.clj
+++ src/bamm/bamm.clj
@@ -12,13 +12,26 @@
    :y 50
    :r 50})
 
-(defn ^:private maybe-emit [{:keys [mag emit? svg-h svg-w]} svg-content]
+(defn ^:private render-legend [legend]
+  (cons
+   [:text {:x 5 :y 15} "LEGEND:"]
+   (reduce-kv
+    (fn [r k v] (conj r [:text {:fill v
+                                :x 5
+                                :y (+ 30 (* 10 (count r)))}
+                         (name k)]))
+    []
+    legend)))
+
+(defn ^:private maybe-emit [legend {:keys [mag emit? svg-h svg-w]} svg-content]
   (if-not emit?
-    svg-content
-    (do (emit
-         (svg {:height (* mag svg-h)
-               :width (* mag svg-w)}
-              svg-content)))))
+    (apply group svg-content)
+    (emit
+     (svg {:height (* mag svg-h)
+           :width (* mag svg-w)}
+          (group
+           (apply group (render-legend legend))
+           (apply group svg-content))))))
 
 (defn tree [category & children]
   {:category category
@@ -33,17 +46,16 @@
      (let [{:keys [mag x y r] :as options} (merge defaults options)
            [left-daughter right-daughter] children]
        (maybe-emit
+        legend
         options
-        (apply
-         group
-         (keep identity
-               [(circle (* mag x) (* mag y) (* mag r)
-                        :fill (get legend category "#FFFFFF"))
-                (when left-daughter
-                  (draw left-daughter legend
-                        (assoc options :x (- x (/ r 2)) :r (/ r 2)
-                               :emit? false)))
-                (when right-daughter
-                  (draw right-daughter legend
-                        (assoc options :x (+ x (/ r 2)) :r (/ r 2)
-                               :emit? false)))]))))))
+        (keep identity
+              [(circle (* mag x) (* mag y) (* mag r)
+                       :fill (get legend category "#FFFFFF"))
+               (when left-daughter
+                 (draw left-daughter legend
+                       (assoc options :x (- x (/ r 2)) :r (/ r 2)
+                              :emit? false)))
+               (when right-daughter
+                 (draw right-daughter legend
+                       (assoc options :x (+ x (/ r 2)) :r (/ r 2)
+                              :emit? false)))])))))
